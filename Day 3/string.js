248b2910ad8fc1b3cc4abd0825fed07025527a07
Akash Singh <121565348+akash9569@users.noreply.github.com>
2025-03-17 08:33:44 +0530

    Today  Task is Completed

---
+++ Day 3/string.js
@@ -0,0 +1,40 @@
+console.log("Todays Topic is String Datatype.");
+
+let name="Akash Singh"
+let age=22
+
+console.log(`Hello My name is ${name} and My age is ${age}.`);
+
+const gameName=new String("iamakashsingh9");
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName[5]);
+console.log(gameName.__proto__);
+
+// Print the Length of String.
+console.log(gameName.length);
+
+// Print the Capital of each character.
+console.log(gameName.toUpperCase());
+
+// Print the small of each character.
+console.log(gameName.toLowerCase());
+
+// Find the character to store the specific position.
+console.log(gameName.charAt(5));
+
+// Find the location of a specific character.
+console.log(gameName.indexOf('m'));
+
+
+// Print the Slicing of Specific word or character.
+console.log(gameName.substring(0,8));
+
+
+
+
+
+
+
+
+
